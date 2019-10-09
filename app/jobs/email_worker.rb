e5cb1c1093ad72a43b8e801a59c5bb0ae591f1c0
Josh <joshsimon85@yahoo.com>
2019-10-09 08:40:41 -0600

    remove private methods from Email Worker

--- app/jobs/email_worker.rb
+++ app/jobs/email_worker.rb
@@ -2,13 +2,11 @@ class EmailWorker < ApplicationJob
   include Sidekiq::Worker
   queue_as :default
   sidekiq_options retry: false, backtrace: true
-  
+
   def perform(email_id)
     send_user_email(email_id)
     send_admin_email(email_id)
   end
-
-  private
 
   def send_user_email(email_id)
     UserMailer.contact_page_email(email_id).deliver
