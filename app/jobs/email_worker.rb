c781081c7f49335aa2e76fc3c836cf7bef42af46
Josh <joshsimon85@yahoo.com>
2019-10-09 17:48:20 -0600

    Fix EmailWorker class

--- app/jobs/email_worker.rb
+++ app/jobs/email_worker.rb
@@ -1,21 +1,22 @@
 class EmailWorker < ApplicationJob
-    queue_as :default
+  queue_as :default
 
-    def perform(type, email_id)
-      if type == 'admin'
-        send_admin_email(email_id)
-      else
-        send_user_email(email_id)
-      end 
+  def perform(type, email_id)
+    if type == 'admin'
+      send_admin_email(email_id)
+    else
+      send_user_email(email_id)
     end
   end
 
   private
 
-  def send_user_email
+  def send_user_email(email_id)
     UserMailer.contact_page_email(email_id).deliver_now
+  end
 
   def send_admin_email(email_id)
     AdminMailer.contact_page_email(email_id).deliver_now
   end
+
 end
