0c2dafe2a7e83177445a7c3812562399f6835f29
Josh <joshsimon85@yahoo.com>
2019-10-08 18:52:50 -0600

    comment out sidekiq initializer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
     :address => ENV['MAILGUN_SMTP_LOGIN'],
