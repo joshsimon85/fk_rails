f8f0112a0e9426ae512f106d7839a03d4b9e6bd9
Josh <joshsimon85@yahoo.com>
2019-10-09 09:16:32 -0600

    upgrade redis

--- app/jobs/email_worker.rb
+++ app/jobs/email_worker.rb
@@ -1,12 +1,13 @@
 class EmailWorker < ApplicationJob
   queue_as :default
-  sidekiq_options backtrace: true
 
   def perform(email_id)
     send_user_email(email_id)
     send_admin_email(email_id)
   end
 
+  private
+  
   def send_user_email(email_id)
     UserMailer.contact_page_email(email_id).deliver
   end
