1be37a12ad894fda22bc6f28141b0e921bb22902
Josh <joshsimon85@yahoo.com>
2019-10-16 07:55:21 -0700

    Add admins path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  
+  def after_sign_in_path_for(resource)
+    binding.pry
+    if resource.admin?
+      admins_path
+    else
+      super
+    end
+  end
 end
