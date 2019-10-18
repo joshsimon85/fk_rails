5b7e68fbfbe71e60841b61ce326ac944f70e22ce
Josh <joshsimon85@yahoo.com>
2019-10-17 19:46:24 -0700

    Finish emails controller tests

--- spec/controllers/email_spec.rb
+++ spec/controllers/email_spec.rb
@@ -143,4 +143,117 @@ RSpec.describe EmailsController do
       end
     end
   end
+
+  describe 'POST destroy' do
+    let(:email) { Fabricate(:email) }
+
+    context 'with authenticated admin' do
+      let(:admin) { Fabricate(:user, admin: true) }
+
+      before(:each) do
+        sign_in(admin, scope: :user)
+        delete :destroy, params: { admin_id: admin.id, id: email.id }
+      end
+
+      it 'sets the flash success message' do
+        expect(flash[:success]).to be_present
+      end
+
+      it 'redirects to the admin emails page and sets the query string to default page 1 and order desc' do
+        expect(response).to redirect_to(admin_emails_path(admin, :order => 'desc', :page => 1))
+      end
+
+      it 'deletes the record from the database' do
+        expect(Email.count).to eq(0)
+      end
+    end
+
+    context 'with authenticated user without admin credentials' do
+      let(:user) { Fabricate(:user) }
+
+      before(:each) do
+        sign_in(user, scope: :user)
+        delete :destroy, params: { admin_id: user.id, id: email.id }
+      end
+
+      it 'redirects to the root path' do
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'sets the flash error message' do
+        expect(flash[:error]).to be_present
+      end
+    end
+
+    context 'with unauthenticated user' do
+      let(:user) { Fabricate(:user) }
+
+      before(:each) do
+        delete :destroy, params: { admin_id: user.id, id: email.id }
+      end
+
+      it 'redirects to the root path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'sets the flash error message' do
+        expect(flash[:alert]).to be_present
+      end
+    end
+  end
+
+  describe 'POST destroy_multiple' do
+    let(:email) { Fabricate(:email) }
+    let(:email_2) { Fabricate(:email) }
+    let(:admin) { Fabricate(:user, admin: true) }
+    let(:user) { Fabricate(:user) }
+
+    context 'with authenticated admin' do
+      before(:each) do
+        sign_in(admin, scope: :user)
+        delete :destroy_multiple, params: { admin_id: admin.id, emails: { "#{email.id}": 'true', "#{email_2.id}": 'true'}}
+      end
+
+      it 'sets the flash success message' do
+        expect(flash[:success]).to be_present
+      end
+
+      it 'redirects to the admin emails page' do
+        expect(response).to redirect_to(admin_emails_path(admin, :order => 'desc', :page => 1))
+      end
+
+      it 'deletes the record from the database' do
+        expect(Email.count).to eq(0)
+      end
+    end
+
+    context 'with authenticated user without admin credentials' do
+      before(:each) do
+        sign_in(user, scope: :user)
+        delete :destroy, params: { admin_id: user.id, id: email.id }
+      end
+
+      it 'redirects to the root path' do
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'sets the flash error message' do
+        expect(flash[:error]).to be_present
+      end
+    end
+
+    context 'with unauthenticated user' do
+      before(:each) do
+        delete :destroy, params: { admin_id: user.id, id: email.id }
+      end
+
+      it 'redirects to the root path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'sets the flash error message' do
+        expect(flash[:alert]).to be_present
+      end
+    end
+  end
 end
