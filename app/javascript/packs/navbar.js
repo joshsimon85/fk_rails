8b95b6f584f3e1f01000fb33cf744fc11a76beec
Josh <joshsimon85@yahoo.com>
2019-11-15 21:49:10 -0700

    Add nav sticky-top

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -23,13 +23,9 @@ $(function() {
     },
     changeNavBarState: function() {
       if ($(window).scrollTop() > 55) {
-        $NAVBAR.addClass('fixed-top enlarged');
-        $SIGNOUTBTN.addClass('lowered');
-        $('body').css('margin-top', '60px');
+        $NAVBAR.addClass('sticky-top');
       } else {
-        $NAVBAR.removeClass('fixed-top enlarged');
-        $SIGNOUTBTN.removeClass('lowered');
-        $('body').removeAttr('style');
+        $NAVBAR.removeClass('sticky-top');
       }
     },
     bindEvents: function() {
