8e7cdecb3d9c181a268208879d35f6ed99559a21
Josh <joshsimon85@yahoo.com>
2019-11-20 09:15:45 -0700

    Action mailer config options

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter     = :sidekiq
   #config.active_job.queue_name_prefix = "rails_franken_kopter_#{Rails.env}"
-  config.action_mailer.default_url_options = { :host => "https://glacial-springs-71454.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => root_path }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
