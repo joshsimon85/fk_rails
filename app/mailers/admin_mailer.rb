8f090f3be6dd056943aa2d951d3291bb3a456d29
Josh <joshsimon85@yahoo.com>
2019-11-21 23:37:07 -0700

    Add from frankenkopter

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,12 +2,12 @@ class AdminMailer < ApplicationMailer
   def contact_page_email(email_id)
     @email = Email.find(email_id)
     @url = 'https://www.frankenkopter.com/contact'
-    mail(to: 'kasey@frankenkopter.com', from: 'mail.frankenkopter.com', reply_to: @email.email, subject: "New email from #{@email.full_name} || FrankenKopter Contact Form")
+    mail(to: 'kasey@frankenkopter.com', from: 'Frankenkopter', reply_to: @email.email, subject: "New email from #{@email.full_name} || FrankenKopter Contact Form")
   end
 
   def new_testimonial_email(user_id)
     @user = User.find(user_id)
     @url = 'https://www.frankenkopter.com/testimonial'
-    mail(to: 'kasey@frankenkopter.com', from: 'mail.frankenkopter.com', reply_to: @user.email, subject: "New testimonial from #{@user.full_name} || FrankenKopter Testimonial")
+    mail(to: 'kasey@frankenkopter.com', from: 'Frankenkopter', reply_to: @user.email, subject: "New testimonial from #{@user.full_name} || FrankenKopter Testimonial")
   end
 end
